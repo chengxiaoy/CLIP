b2015198d63dc90b888fbb6f54eb5dd4fb68317b
不爱狗的人 <852661135@qq.com>
2022-07-24 08:19:36 +0800

    for torchscript

--- clip/model.py
+++ clip/model.py
@@ -159,13 +159,6 @@ class ModifiedResNet(nn.Module):
         return x
 
 
-class LayerNorm(nn.LayerNorm):
-    """Subclass torch's LayerNorm to handle fp16."""
-
-    def forward(self, x: torch.Tensor):
-        orig_type = x.dtype
-        ret = super().forward(x.type(torch.float32))
-        return ret.type(orig_type)
 
 
 class LayerNorm(nn.Module):
